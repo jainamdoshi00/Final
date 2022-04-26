64264aa0e184b14f8fb46242bf40377aa428610c
Jainam Doshi <jainamdoshi00@gmail.com>
2022-04-26 23:19:23 +0530

    M

--- Add.java
+++ Add.java
@@ -2,9 +2,9 @@ public class Add
 {
 public static void main(String args[])
 {
-int n1 = integer.parseInt(args[0]), n2 = integer.parseInt(args[1]),sum;
-System.out.println(n1+""+n2);
-sum=n1+n2;
+int n1 = integer.parseInt(args[0]);
+int n2 = integer.parseInt(args[1]);
+int sum=n1+n2;
 System.out.println(sum);
 }
 }
